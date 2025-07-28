19d2a460ab221447a096fbc0c02a8c414c271433
jdiaconu <jdiaconu@cisco.com>
2025-07-28 09:48:16 +0200

    feat: Second rename
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- python/identityservice/__main__.py
+++ python/identityservice/__main__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Main cli for the Identity Service Python SDK."""
 
-from identityplatform import cli
+from identityservice import cli
 
 if __name__ == "__main__":
     cli.app()
